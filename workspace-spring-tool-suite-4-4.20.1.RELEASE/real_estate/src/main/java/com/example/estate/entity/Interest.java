fd464be2ff65f3f67a8389794652767dde701265
Suno7526 <showme314@naver.com>
2024-02-16 16:24:14 +0900

    Interest 테이블 추가

--- workspace-spring-tool-suite-4-4.20.1.RELEASE/real_estate/src/main/java/com/example/estate/entity/Interest.java
+++ workspace-spring-tool-suite-4-4.20.1.RELEASE/real_estate/src/main/java/com/example/estate/entity/Interest.java
@@ -10,4 +10,16 @@ import jakarta.persistence.ManyToOne;
 @Entity
 public class Interest {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long interestCode;
+	
+	@ManyToOne
+	@JoinColumn(name = "userCode")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "productCode")
+	private Product product;
+	
 }
