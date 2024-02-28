5be59ea994daf373e634a902a21151d52f4f1684
Suno7526 <showme314@naver.com>
2024-02-28 17:38:29 +0900

    메인홈페이지 추가

--- estate/src/App.js
+++ estate/src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Join from './Pages/Join';
 import Home from './Pages/Home';
 import ProductJoin from './Pages/ProductJoin';
 
 function App() {
-
   return (
-
     <BrowserRouter>
-        
-        <Link to="/ProductJoin">매물등록</Link><br></br>
-        <Link to="/Home">홈</Link><br></br>
-        <Link to="/Join">회원가입</Link>
-        <Routes>
-          <Route path="/Join" element={<Join />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/ProductJoin" element={<ProductJoin />} />
-        </Routes>
+      <Link to="/ProductJoin">매물등록</Link>
+      <br></br>
+      <Link to="/Home">홈</Link>
+      <br></br>
+      <Link to="/Join">회원가입</Link>
+      <Routes>
+        <Route path="/Join" element={<Join />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/ProductJoin" element={<ProductJoin />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
