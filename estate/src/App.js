04050116e7c32e89508517df27e1d6112a75d986
Suno7526 <showme314@naver.com>
2024-04-09 16:22:56 +0900

    test

--- estate/src/App.js
+++ estate/src/App.js
@@ -14,7 +14,7 @@ import { useState, useEffect } from 'react';
 import Footer from './Components/Footer';
 import Body from './Components/Body';
 import Question from './Pages/Question';
-
+//123124124
 function App() {
   const [isLogin, setIsLogin] = useState(false); // 로그인 여부 관리
 
