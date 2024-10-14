6a3490fe5b10840cf5c17cb6da604ea8f1e29a0e
Yoon <Yoon@naver.com>
2024-10-14 23:40:25 +0900

    HOME 채팅방 수정

--- estate/src/App.js
+++ estate/src/App.js
@@ -75,13 +75,13 @@ function App() {
       ChannelIO('boot', {
         "pluginKey": "49f16ab4-ee61-4945-90b6-055d91c92119",
         "memberId": "${sessionStorage.getItem(
-        'userEmail',
-    )}" || '', // fill user's member id
+          'userEmail',
+        )}" || '', // fill user's member id
         "profile": { // fill user's profile
           "name": "${sessionStorage.getItem('userName')}", // fill user's name
           "mobileNumber": "${sessionStorage.getItem(
-        'userPhone',
-    )}", // fill user's mobile number
+            'userPhone',
+          )}", // fill user's mobile number
           "landlineNumber": "USER_LANDLINE_NUMBER", // fill user's landline number  
           "CUSTOM_VALUE_1": "VALUE_1", // custom property
           "CUSTOM_VALUE_2": "VALUE_2" // custom property
@@ -101,93 +101,91 @@ function App() {
   };
 
   return (
-      <BrowserRouter>
-        <Header />
-        <Body />
+    <BrowserRouter>
+      <Header />
+      <Body />
 
-        {/* 채팅 열기/닫기 버튼 */}
-        <button
-            style={{
-              position: 'fixed',  // 화면에 고정
-              bottom: '20px',     // 화면 하단으로부터 20px 위에 위치
-              right: '100px',     // 화면 오른쪽으로부터 100px 왼쪽에 위치
-              padding: '10px 20px',
-              backgroundColor: 'blue',
-              color: 'white',
-              border: 'none',
-              borderRadius: '5px',
-              cursor: 'pointer',
-              zIndex: '1000',      // 다른 요소들 위에 표시되도록 설정
-            }}
-            onClick={toggleChat}
+      {/* 채팅 열기/닫기 버튼 */}
+      <button
+        style={{
+          position: 'fixed', // 화면에 고정
+          bottom: '20px', // 화면 하단으로부터 20px 위에 위치
+          right: '100px', // 화면 오른쪽으로부터 100px 왼쪽에 위치
+          padding: '10px 20px',
+          backgroundColor: 'blue',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          zIndex: '1000', // 다른 요소들 위에 표시되도록 설정
+        }}
+        onClick={toggleChat}
+      >
+        {isChatVisible ? '채팅 닫기' : '채팅 열기'}
+      </button>
+
+      {/* 채팅 창 - 페이지 스크롤에도 고정 */}
+      {isChatVisible && (
+        <div
+          style={{
+            padding: '10px',
+            position: 'fixed', // 화면에 고정
+            bottom: '80px', // 화면 하단으로부터 80px 위에 위치
+            right: '20px', // 화면 오른쪽으로부터 20px 왼쪽에 위치
+            width: '850px',
+            height: '650px',
+            borderRadius: '5px',
+            overflowY: 'auto', // 채팅 내용이 넘치면 스크롤
+            zIndex: '1000', // 다른 요소들 위에 표시되도록 설정
+          }}
         >
-          {isChatVisible ? '채팅 닫기' : '채팅 열기'}
-        </button>
+          <Chat />
+        </div>
+      )}
 
-        {/* 채팅 창 - 페이지 스크롤에도 고정 */}
-        {isChatVisible && (
-            <div
-                style={{
-                  padding: '10px',
-                  position: 'fixed',  // 화면에 고정
-                  bottom: '80px',     // 화면 하단으로부터 80px 위에 위치
-                  right: '20px',      // 화면 오른쪽으로부터 20px 왼쪽에 위치
-                  width: '300px',
-                  height: '1000px',
-                  backgroundColor: 'white',
-                  border: '1px solid #ccc',
-                  borderRadius: '5px',
-                  overflowY: 'auto',   // 채팅 내용이 넘치면 스크롤
-                  zIndex: '1000',      // 다른 요소들 위에 표시되도록 설정
-                }}
-            >
-              <Chat />
-            </div>
-        )}
+      <Routes>
+        <Route path="/Like" element={<Like />} />
+        <Route path="/RecentItem" element={<RecentItem />} />
+        <Route path="/UserLogin" element={<UserLogin />} />
+        <Route path="/Join" element={<Join />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/ProductJoin" element={<ProductJoin />} />
+        <Route path="/product/:productCode" element={<Product />} />
+        <Route path="/Category/:category" element={<Category />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Mypage" element={<Mypage />} />
+        <Route path="/Question" element={<Question />} />
+        <Route path="/Inquiry" element={<Inquiry />} />
+        <Route path="/BestFAQ" element={<BestFAQ />} />
+        <Route path="/WithdrawETC" element={<WithdrawETC />} />
+        <Route path="/QLoginInfo" element={<QLoginInfo />} />
+        <Route path="/QProduct" element={<QProduct />} />
+        <Route path="/QDelivery" element={<QDelivery />} />
+        <Route path="/QCancel" element={<QCancel />} />
+        <Route path="/QExchange" element={<QExchange />} />
+        <Route path="/Review/:orderCode" element={<Review />} />
+        <Route path="/ProductUpdate" element={<ProductUpdate />} />
+        <Route path="/MyInquiry" element={<MyInquiry />} />
+        <Route path="/Search/:query" element={<Search />} />
+        <Route path="/MyQuestion/:questionCode" element={<MyQuestion />} />
+        <Route path="/Payment" element={<Payment />} />
+        <Route path="/PaymentProduct" element={<PaymentProduct />} />
+        <Route path="/ProductImages" element={<ProductImages />} />
+        <Route path="/Main" element={<Main />} />
+        <Route path="/MypageUser" element={<MypageUser />} />
+        <Route path="/CouponCreate" element={<CouponCreate />} />
+        <Route path="/CouponAccept" element={<CouponAccept />} />
+        <Route path="/Refund" element={<Refund />} />
+        <Route path="/Chat" element={<Chat />} />
+        <Route path="/UserUpdate" element={<UserUpdate />} />
+        <Route
+          path="/DeliveryTracking/:orderCode"
+          element={<DeliveryTracking />}
+        />
+      </Routes>
 
-        <Routes>
-          <Route path="/Like" element={<Like />} />
-          <Route path="/RecentItem" element={<RecentItem />} />
-          <Route path="/UserLogin" element={<UserLogin />} />
-          <Route path="/Join" element={<Join />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/ProductJoin" element={<ProductJoin />} />
-          <Route path="/product/:productCode" element={<Product />} />
-          <Route path="/Category/:category" element={<Category />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/Mypage" element={<Mypage />} />
-          <Route path="/Question" element={<Question />} />
-          <Route path="/Inquiry" element={<Inquiry />} />
-          <Route path="/BestFAQ" element={<BestFAQ />} />
-          <Route path="/WithdrawETC" element={<WithdrawETC />} />
-          <Route path="/QLoginInfo" element={<QLoginInfo />} />
-          <Route path="/QProduct" element={<QProduct />} />
-          <Route path="/QDelivery" element={<QDelivery />} />
-          <Route path="/QCancel" element={<QCancel />} />
-          <Route path="/QExchange" element={<QExchange />} />
-          <Route path="/Review/:orderCode" element={<Review />} />
-          <Route path="/ProductUpdate" element={<ProductUpdate />} />
-          <Route path="/MyInquiry" element={<MyInquiry />} />
-          <Route path="/Search/:query" element={<Search />} />
-          <Route path="/MyQuestion/:questionCode" element={<MyQuestion />} />
-          <Route path="/Payment" element={<Payment />} />
-          <Route path="/PaymentProduct" element={<PaymentProduct />} />
-          <Route path="/ProductImages" element={<ProductImages />} />
-          <Route path="/Main" element={<Main />} />
-          <Route path="/MypageUser" element={<MypageUser />} />
-          <Route path="/CouponCreate" element={<CouponCreate />} />
-          <Route path="/CouponAccept" element={<CouponAccept />} />
-          <Route path="/Refund" element={<Refund />} />
-          <Route path="/Chat" element={<Chat />} />
-          <Route path="/UserUpdate" element={<UserUpdate />} />
-          <Route
-              path="/DeliveryTracking/:orderCode"
-              element={<DeliveryTracking />}
-          />
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
