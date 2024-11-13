3882c311d07d2cc91b521b946e3a4b8f64a71061
Suno7526 <showme314@naver.com>
2024-11-13 17:43:17 +0900

    상품등록 시 색상, 사이즈입력 가능하도록 변경
    기능구현은 아직 안됨

--- estate/src/Pages/ProductJoin.js
+++ estate/src/Pages/ProductJoin.js
@@ -11,10 +11,15 @@ const ProductJoin = () => {
     companyName: '',
     productStuck: '',
     category: '',
+    colors: [],  // 색상 배열
+    sizes: [],   // 사이즈 배열
   });
 
   const [productImages, setProductImages] = useState([]);
   const [previewImages, setPreviewImages] = useState([]);
+
+  const [newColor, setNewColor] = useState('');
+  const [newSize, setNewSize] = useState('');
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -32,20 +37,42 @@ const ProductJoin = () => {
     setPreviewImages([...previewImages, ...imagePreviews]);
   };
 
+  const handleAddColor = () => {
+    if (newColor) {
+      setProductData({
+        ...productData,
+        colors: [...productData.colors, newColor],
+      });
+      setNewColor('');
+    }
+  };
+
+  const handleAddSize = () => {
+    if (newSize) {
+      setProductData({
+        ...productData,
+        sizes: [...productData.sizes, newSize],
+      });
+      setNewSize('');
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       if (
-        !productData.productName ||
-        !productData.information ||
-        !productData.productPrice ||
-        !productData.companyName ||
-        !productData.productStuck ||
-        !productData.category ||
-        productImages.length === 0
+          !productData.productName ||
+          !productData.information ||
+          !productData.productPrice ||
+          !productData.companyName ||
+          !productData.productStuck ||
+          !productData.category ||
+          productImages.length === 0 ||
+          productData.colors.length === 0 ||
+          productData.sizes.length === 0
       ) {
         alert(
-          '상품명, 설명, 가격, 회사명, 재고, 상품 카테고리, 그리고 이미지를 모두 입력해주세요.',
+            '상품명, 설명, 가격, 회사명, 재고, 상품 카테고리, 색상, 사이즈 그리고 이미지를 모두 입력해주세요.',
         );
         return;
       }
@@ -57,6 +84,10 @@ const ProductJoin = () => {
       formData.append('companyName', productData.companyName);
       formData.append('productStuck', productData.productStuck);
       formData.append('category', productData.category);
+
+      // 색상과 사이즈도 formData에 추가
+      productData.colors.forEach(color => formData.append('colors', color));
+      productData.sizes.forEach(size => formData.append('sizes', size));
 
       productImages.forEach((image, index) => {
         formData.append('productImages', image);
@@ -74,144 +105,176 @@ const ProductJoin = () => {
   };
 
   return (
-    <div className="App">
-      <ManageAside />
-      <div id="propertyform">
-        <h2>상품 등록</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="productName" className="productJoin-label">
-            상품명:
-          </label>
-          <input
-            type="text"
-            id="productName"
-            name="productName"
-            value={productData.productName}
-            onChange={handleInputChange}
-            className="productJoin-input"
-            required
-          />
-          <br />
+      <div className="App">
+        <ManageAside />
+        <div id="propertyform">
+          <h2>상품 등록</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="productName" className="productJoin-label">
+              상품명:
+            </label>
+            <input
+                type="text"
+                id="productName"
+                name="productName"
+                value={productData.productName}
+                onChange={handleInputChange}
+                className="productJoin-input"
+                required
+            />
+            <br />
 
-          <label htmlFor="information">설명:</label>
-          <textarea
-            id="information"
-            name="information"
-            value={productData.information}
-            onChange={handleInputChange}
-            className="productJoin-input"
-            required
-          />
-          <br />
+            <label htmlFor="information">설명:</label>
+            <textarea
+                id="information"
+                name="information"
+                value={productData.information}
+                onChange={handleInputChange}
+                className="productJoin-input"
+                required
+            />
+            <br />
 
-          <label htmlFor="productPrice">가격:</label>
-          <input
-            type="number"
-            id="productPrice"
-            name="productPrice"
-            value={productData.productPrice}
-            onChange={handleInputChange}
-            className="productJoin-input"
-            required
-          />
-          <br />
+            <label htmlFor="productPrice">가격:</label>
+            <input
+                type="number"
+                id="productPrice"
+                name="productPrice"
+                value={productData.productPrice}
+                onChange={handleInputChange}
+                className="productJoin-input"
+                required
+            />
+            <br />
 
-          <label htmlFor="companyName">회사명:</label>
-          <input
-            type="text"
-            id="companyName"
-            name="companyName"
-            value={productData.companyName}
-            onChange={handleInputChange}
-            className="productJoin-input"
-            required
-          />
-          <br />
+            <label htmlFor="companyName">회사명:</label>
+            <input
+                type="text"
+                id="companyName"
+                name="companyName"
+                value={productData.companyName}
+                onChange={handleInputChange}
+                className="productJoin-input"
+                required
+            />
+            <br />
 
-          <label htmlFor="productStuck">재고:</label>
-          <input
-            type="number"
-            id="productStuck"
-            name="productStuck"
-            value={productData.productStuck}
-            onChange={handleInputChange}
-            className="productJoin-input"
-            required
-          />
-          <br />
+            <label htmlFor="productStuck">재고:</label>
+            <input
+                type="number"
+                id="productStuck"
+                name="productStuck"
+                value={productData.productStuck}
+                onChange={handleInputChange}
+                className="productJoin-input"
+                required
+            />
+            <br />
 
-          <label htmlFor="category">카테고리:</label>
-          <select
-            id="category"
-            name="category"
-            value={productData.category}
-            onChange={handleInputChange}
-            className="productJoin-input"
-            required
-          >
-            <option value="">카테고리 선택</option>
-            <optgroup label="아우터">
-              <option value="재킷">재킷</option>
-              <option value="집업">집업</option>
-              <option value="점퍼">점퍼</option>
-              <option value="코트">코트</option>
-              <option value="패딩/파카">패딩/파카</option>
-              <option value="모피/머스탱">모피/머스탱</option>
-            </optgroup>
-            <optgroup label="상의">
-              <option value="민소매">민소매</option>
-              <option value="조끼">조끼</option>
-              <option value="반팔티">반팔티</option>
-              <option value="긴팔티">긴팔티</option>
-              <option value="셔츠">셔츠</option>
-              <option value="크루넥">크루 넥</option>
-              <option value="니트">니트</option>
-              <option value="후드">후드</option>
-            </optgroup>
-            <optgroup label="하의">
-              <option value="반바지">반바지</option>
-              <option value="츄리닝">츄리닝</option>
-              <option value="긴바지">긴바지</option>
-              <option value="치마">치마</option>
-            </optgroup>
-            <optgroup label="모자">
-              <option value="캡">캡</option>
-              <option value="버킷햇">버킷햇</option>
-              <option value="스냅백">스냅백</option>
-              <option value="비니">비니</option>
-              <option value="기타">기타</option>
-            </optgroup>
-          </select>
-          <br />
+            <label htmlFor="category">카테고리:</label>
+            <select
+                id="category"
+                name="category"
+                value={productData.category}
+                onChange={handleInputChange}
+                className="productJoin-input"
+                required
+            >
+              <option value="">카테고리 선택</option>
+              <optgroup label="아우터">
+                <option value="재킷">재킷</option>
+                <option value="집업">집업</option>
+                <option value="점퍼">점퍼</option>
+                <option value="코트">코트</option>
+                <option value="패딩/파카">패딩/파카</option>
+                <option value="모피/머스탱">모피/머스탱</option>
+              </optgroup>
+              <optgroup label="상의">
+                <option value="민소매">민소매</option>
+                <option value="조끼">조끼</option>
+                <option value="반팔티">반팔티</option>
+                <option value="긴팔티">긴팔티</option>
+                <option value="셔츠">셔츠</option>
+                <option value="크루넥">크루 넥</option>
+                <option value="니트">니트</option>
+                <option value="후드">후드</option>
+              </optgroup>
+              <optgroup label="하의">
+                <option value="반바지">반바지</option>
+                <option value="츄리닝">츄리닝</option>
+                <option value="긴바지">긴바지</option>
+                <option value="치마">치마</option>
+              </optgroup>
+              <optgroup label="모자">
+                <option value="캡">캡</option>
+                <option value="버킷햇">버킷햇</option>
+                <option value="스냅백">스냅백</option>
+                <option value="비니">비니</option>
+                <option value="기타">기타</option>
+              </optgroup>
+            </select>
+            <br />
 
-          <label htmlFor="productImages">상품 이미지:</label>
-          <input
-            type="file"
-            id="productImages"
-            name="productImages"
-            multiple
-            onChange={handleFileChange}
-            required
-          />
-          <br />
-
-          {previewImages.length > 0 && (
-            <div className="image-preview">
-              {previewImages.map((src, index) => (
-                <img
-                  key={index}
-                  src={src}
-                  alt={`preview ${index}`}
-                  style={{ width: '200px', height: '200px', marginTop: '10px' }}
-                />
+            <label htmlFor="newColor">색상:</label>
+            <input
+                type="text"
+                id="newColor"
+                value={newColor}
+                onChange={(e) => setNewColor(e.target.value)}
+                className="productJoin-input"
+            />
+            <button type="button" onClick={handleAddColor}>색상 추가</button>
+            <ul>
+              {productData.colors.map((color, index) => (
+                  <li key={index}>{color}</li>
               ))}
-            </div>
-          )}
+            </ul>
+            <br />
 
-          <button type="submit">상품 등록</button>
-        </form>
+            <label htmlFor="newSize">사이즈:</label>
+            <input
+                type="text"
+                id="newSize"
+                value={newSize}
+                onChange={(e) => setNewSize(e.target.value)}
+                className="productJoin-input"
+            />
+            <button type="button" onClick={handleAddSize}>사이즈 추가</button>
+            <ul>
+              {productData.sizes.map((size, index) => (
+                  <li key={index}>{size}</li>
+              ))}
+            </ul>
+            <br />
+
+            <label htmlFor="productImages">상품 이미지:</label>
+            <input
+                type="file"
+                id="productImages"
+                name="productImages"
+                multiple
+                onChange={handleFileChange}
+                required
+            />
+            <br />
+
+            {previewImages.length > 0 && (
+                <div className="image-preview">
+                  {previewImages.map((src, index) => (
+                      <img
+                          key={index}
+                          src={src}
+                          alt={`preview ${index}`}
+                          style={{ width: '200px', height: '200px', marginTop: '10px' }}
+                      />
+                  ))}
+                </div>
+            )}
+
+            <button type="submit">상품 등록</button>
+          </form>
+        </div>
       </div>
-    </div>
   );
 };
 
